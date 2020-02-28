d5acfc878db9cd7c5218af116203d960b70ae18f
Bruce Spang <bruce@brucespang.com>
2020-02-27 16:00:09 -0800

    Flush stdout before opening trace because otherwise it doesn't seem to work until you hit enter

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tcp_probe", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Bruce Spang",
     author_email="bruce@brucespang.com",
     description="Tools for tracing TCP in Linux",
--- tcp_probe/cli.py
+++ tcp_probe/cli.py
@@ -23,7 +23,7 @@ def set_trace_filter(event, filter_string):
     f = open("/sys/kernel/debug/tracing/events/%s/filter"%event, mode='w')
     f.write(filter_string)
     f.close()
-
+    
 def clear_trace_buffer():
     subprocess.call('echo "" > /sys/kernel/debug/tracing/trace', shell=True)
 
@@ -47,29 +47,33 @@ def plot(output_dir, normalize_time, trace_path):
 @cli.command()
 @click.option('--clear/--no-clear', default=True, help="Whether to clear the kernel trace buffer before starting the trace")
 @click.option('-b', '--buffer', 'buffer_size', default=500000, help="Size of trace buffer in kb")
-@click.argument('filter_string', metavar='filter', default='', callback=validate_filter)
+@click.argument('filter_string', metavar='filter', default='0', callback=validate_filter)
 def trace(clear, buffer_size, filter_string):
     """Takes a tcp-related trace from the Linux kernel.
 
     Takes an optional filter for the kernel trace. Please see the kernel documentation for syntax: https://www.kernel.org/doc/html/latest/trace/events.html#event-filtering
     """
+    events = [
+        "tcp"
+    ]
+    
     try:
-        enable_event("tcp")
-        # enable_event("tcp/tcp_probe")
-        # enable_event("tcp/tcp_retransmit_skb")
-        # enable_event("tcp/tcp_retransmit_synack")
+        for event in events:
+            set_trace_filter(event, filter_string)
+            enable_event(event)
     except PermissionError as e:
         print("error: insufficient permissions to enable event tracing", file=sys.stderr)
         sys.exit(1)
 
     set_trace_buffer_size(buffer_size)
-    set_trace_filter("tcp", filter_string)
 
     if clear:
         clear_trace_buffer()
 
     try:
+        print(flush=True)
         subprocess.call('cat /sys/kernel/debug/tracing/trace_pipe', shell=True)
     finally:
-        disable_event("tcp")
-        set_trace_filter("tcp", "")
+        for event in events:
+            disable_event(event)
+            set_trace_filter(event, "0")
